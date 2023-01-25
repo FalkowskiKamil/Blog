137ea7181ac00f04ff2312a7d3401629c65f9b9a
Kamil Falkowski <falkowskikamil97@wp.pl>
2023-01-25 15:00:37 +0100

    Made Base

--- blog/routes.py
+++ blog/routes.py
@@ -1,9 +1,6 @@
 from flask import render_template
-from __init__ import app
+from blog import app
 
-@app.route('/')
+@app.route("/")
 def index():
    return render_template("base.html")
-
-if __name__ == "__main__":
-   app.run(debug=True)
